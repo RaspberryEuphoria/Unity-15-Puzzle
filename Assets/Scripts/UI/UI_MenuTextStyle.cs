288e9b17933a913d57005171f5f57c1ba652e103
RaspberryEuphoria <manon.dev.mion@gmail.com>
2021-11-01 15:26:57 +0100

    feat: improve UI_MenuTextStyle script

---
+++ Assets/Scripts/UI/UI_MenuTextStyle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MenuTextStyle : MonoBehaviour
+{
+    public bool isHover;
+    public Color hovercolor = new Color(1, 0.5f, 1, 1);
+
+    private TextMesh textmesh;
+    private Color defaultColor;
+
+    private void Start()
+    {
+        textmesh = GetComponent<TextMesh>();
+        defaultColor = textmesh.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        isHover = true;
+        //textmesh.fontStyle = FontStyle.Bold;
+        // textmesh.color = TextMesh.color
+
+        StartCoroutine(SetTextTransparency(hovercolor));
+    }
+
+    private void OnMouseExit()
+    {
+        isHover = false;
+        //textmesh.fontStyle = FontStyle.Normal;
+
+        StartCoroutine(SetTextTransparency(defaultColor));
+    }
+
+    IEnumerator SetTextTransparency(Color endColor)
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            textmesh.color = Color32.Lerp(textmesh.color, endColor, t);
+            t += Time.deltaTime / 0.5f;
+
+            yield return null;
+        }
+
+    }
+}
--- Assets/Scripts/UI/UI_PlayHandler.cs
+++ Assets/Scripts/UI/UI_PlayHandler.cs
@@ -4,31 +4,10 @@ using UnityEngine;
 
 public class UI_PlayHandler : MonoBehaviour
 {
-    public bool isHover;
     public LaunchGame launcher;
-
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void OnMouseEnter()
-    {
-        isHover = true;
-    }
 
     void OnMouseDown()
     {
-        isHover = false;
         launcher.StartNewGame("15Puzzle");
-    }
-
-    void OnMouseExit()
-    {
-        isHover = false;
     }
 }
